d796dfa3d3319dfc0db069ce0054379ee149b088
MrSquatch <86651695+MrSquatch@users.noreply.github.com>
2024-06-30 02:29:04 -0500

    test: fixing spacing error

--- src/components/TestButton/TestButton.jsx
+++ src/components/TestButton/TestButton.jsx
@@ -8,20 +8,18 @@ const AdjacencyListButton = () => {
 
   // Función DFS limitada con Map para control de visitados
   const dfsLimited = (adjList, currentNode, strRes, depth, visited) => {
-    visited.set(currentNode, true); // Marcar el nodo como visitado
-
-    strRes += adjList[currentNode].name; // Agregar el nombre del nodo al resultado
+    visited.set(currentNode, true);
+    strRes += adjList[currentNode].name + ' ';
 
     if (depth === 2) {
-      console.log(strRes); // Imprimir resultado cuando se alcanza la profundidad deseada
+      console.log(strRes);
       return;
     }
 
     // Recorrer los hijos del nodo actual
     adjList[currentNode].children.forEach((child) => {
       if (!visited.has(child.index)) {
-        // Si el hijo no ha sido visitado
-        dfsLimited(adjList, child.index, strRes, depth + 1, visited); // Llamada recursiva para el siguiente nivel
+        dfsLimited(adjList, child.index, strRes, depth + 1, visited);
       }
     });
   };
@@ -29,11 +27,8 @@ const AdjacencyListButton = () => {
   const handleAdjacencyListClick = () => {
     console.log(adjacencyList);
 
-    // Aquí podrías hacer más con la lista de adyacencia, como mostrarla en la interfaz o procesarla
-
-    const visited = new Map(); // Map para rastrear los nodos visitados
-
-    dfsLimited(adjacencyList, 0, '', 0, visited); // Llamar a DFS desde el nodo raíz (índice 0), profundidad inicial 0
+    const visited = new Map();
+    dfsLimited(adjacencyList, 0, '', 0, visited);
   };
 
   return (
